2c528588857235e5103dfa3fe536e0d950b937f1
muthdra <gabrielkwi@hotmail.com>
2014-10-19 22:20:17 -0200

    Using Classes - Stable

--- fonte/Code.java
+++ fonte/Code.java
@@ -1,35 +1,11 @@
-import java.util.Stack;
-
-public class Code{
-	private String code;
-	private int[] matchingBrackets;
-	
-	Code(String c){
-		code = c;
+class IllC{
+	public static void main(String args[]){
+		String codeToInterpret = "";
 		
-		matchingBrackets = new int[code.length()];
-		Stack<Integer> matchingBracketsStack = new Stack<Integer>();
-		int i;
-		for(i = 0; i < code.length(); i++){
-			if(code.charAt(i) == '['){
-				matchingBracketsStack.push(new Integer(i));
-			}
-			if(code.charAt(i) == ']'){
-				matchingBrackets[i] = ((Integer)matchingBracketsStack.peek()).intValue();
-				matchingBrackets[((Integer)matchingBracketsStack.pop()).intValue()] = i;
-			}
-		}
-	}
-	
-	public String getCode(){
-		return code;
-	}
-	
-	public int getMatchingBracket(int m){
-		return matchingBrackets[m];
-	}
-	
-	public int getLength(){
-		return code.length();
+		Code codeToEval = new Code(codeToInterpret);
+		
+		IllArray cellArray = new IllArray(codeToEval, 30000);
+		
+		IllInstruction.execute(codeToEval, cellArray);
 	}
 }

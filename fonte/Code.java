7e1b9b4cb4e544bd5d8375eebf0a9e89af350975
muthdra <gabrielkwi@hotmail.com>
2014-10-19 22:22:16 -0200

    Correct upload of last version

--- fonte/Code.java
+++ fonte/Code.java
@@ -1,11 +1,42 @@
-class IllC{
-	public static void main(String args[]){
-		String codeToInterpret = "";
+import java.util.Stack;
+
+public class Code{
+	private String code;
+	private int[] matchingBrackets;
+	private int instructionPointer;
+	
+	Code(String c){
+		code = c;
 		
-		Code codeToEval = new Code(codeToInterpret);
+		instructionPointer = 0;
 		
-		IllArray cellArray = new IllArray(codeToEval, 30000);
-		
-		IllInstruction.execute(codeToEval, cellArray);
+		matchingBrackets = new int[code.length()];
+		Stack<Integer> matchingBracketsStack = new Stack<Integer>();
+		int i;
+		for(i = 0; i < code.length(); i++){
+			if(code.charAt(i) == '['){
+				matchingBracketsStack.push(new Integer(i));
+			}
+			if(code.charAt(i) == ']'){
+				matchingBrackets[i] = ((Integer)matchingBracketsStack.peek()).intValue();
+				matchingBrackets[((Integer)matchingBracketsStack.pop()).intValue()] = i;
+			}
+		}
+	}
+	
+	public String getCode(){
+		return code;
+	}
+	
+	public int getInstructionPointer(){
+		return instructionPointer;
+	}
+	
+	public void setInstructionPointer(int i){
+		instructionPointer = i;
+	}
+	
+	public int getMatchingBracket(int m){
+		return matchingBrackets[m];
 	}
 }

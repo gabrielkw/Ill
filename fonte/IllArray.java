df5bbd10ec5412c78c65bfff8bc3934e816dec71
muthdra <gabrielkwi@hotmail.com>
2014-10-19 22:20:52 -0200

    First tested stable version

--- fonte/IllArray.java
+++ fonte/IllArray.java
@@ -12,4 +12,20 @@ public class IllArray{
 			cells[i] = 0;
 		}
 	}
+	
+	public int getIndicator(){
+		return indicator;
+	}
+	
+	public void setIndicator(int i){
+		indicator = i;
+	}
+	
+	public int getCellValue(int c){
+		return cells[c];
+	}
+	
+	public void setCellValue(int cell, int value){
+		cells[cell] = value;
+	}
 }
